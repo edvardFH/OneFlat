d15877e15f2e53183f4ed798c127d4a27299e8db
edvardFH <97302653+edvardFH@users.noreply.github.com>
2024-05-13 11:30:00 +0200

    Logging availability check

--- reservation-management-service/src/main/java/com/onesquad/reservation/ReservationController.java
+++ reservation-management-service/src/main/java/com/onesquad/reservation/ReservationController.java
@@ -1,6 +1,7 @@
 package com.onesquad.reservation;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/reservation")
 @AllArgsConstructor
+@Slf4j
 public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping(path = "{accommodationId}")
     public AvailabilityCheckResponse isAvailable(@PathVariable("accommodationId") Long accommodationId) {
+        log.info("Checking availability for accommodation id {}", accommodationId);
+
         boolean isAvailable = reservationService.isAvailable(accommodationId);
         return new AvailabilityCheckResponse(isAvailable);
     }
